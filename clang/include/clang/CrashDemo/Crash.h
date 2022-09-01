e48f6c99597ff908377e7f2ff8fc5f49b8455834
Michael Buch <michaelbuch12@gmail.com>
2022-09-01 17:46:36 +0100

    Reduce ASTImporter-demo-crashing

--- clang/include/clang/CrashDemo/Crash.h
+++ clang/include/clang/CrashDemo/Crash.h
@@ -2,23 +2,11 @@
 #include "clang/AST/ASTImportError.h"
 #include "clang/AST/ExprCXX.h"
 namespace clang {
-class ASTImporterSharedState;
-class FileManager;
   class Crasher {
-    enum class ODRHandlingType { Conservative, Liberal };
     class ImportPathTy {
       llvm::SmallDenseMap<Decl *, int, 32> Aux;
     };
-    std::shared_ptr<ASTImporterSharedState> SharedState = nullptr;
     ImportPathTy ImportPath;
-    ASTContext *ToContext;
-    ASTContext *FromContext;
-    FileManager *ToFileManager;
-    FileManager *FromFileManager;
-    bool Minimal;
-    ODRHandlingType ODRHandling;
-    llvm::DenseMap<Decl *, Decl *> ImportedDecls;
-  public:
     template <typename ImportT>
     [[nodiscard]] llvm::Error importInto(ImportT &To, const ImportT &From) {}
     llvm::Expected<ExprWithCleanups::CleanupObject>
--- clang/tools/ASTImporterDemo-crashing.cpp
+++ clang/tools/ASTImporterDemo-crashing.cpp
@@ -3,7 +3,6 @@
 
 int main() {
   std::unique_ptr<clang::ASTUnit> ToUnit = nullptr;
-  std::unique_ptr<clang::ASTUnit> FromUnit = nullptr;
   clang::Crasher Importer;
   return 0;
 }

9e5589873126d2430da4dfb40c95a3ba404b969a
Michael Buch <michaelbuch12@gmail.com>
2022-09-02 01:46:01 +0100

    Reduce ASTImporter-demo-crashing

--- clang/include/clang/CrashDemo/Crash.h
+++ clang/include/clang/CrashDemo/Crash.h
@@ -1,5 +1,4 @@
 #ifndef LLVM_CLANG_CRASHDEMO_CRASH_H
-#include "clang/AST/ASTImportError.h"
 #include "clang/AST/ExprCXX.h"
 namespace clang {
   class Crasher {
@@ -7,10 +6,10 @@ namespace clang {
       llvm::SmallDenseMap<Decl *, int, 32> Aux;
     };
     ImportPathTy ImportPath;
-    template <typename ImportT>
-    [[nodiscard]] llvm::Error importInto(ImportT &To, const ImportT &From) {}
-    llvm::Expected<ExprWithCleanups::CleanupObject>
+
+    ExprWithCleanups::CleanupObject
     Import(ExprWithCleanups::CleanupObject From);
+
   };
 } // namespace clang
 #endif // _H

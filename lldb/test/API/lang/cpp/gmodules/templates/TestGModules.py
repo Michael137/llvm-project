d94bdf8fef9967d2cd6556a53d7bc194cd7effc6
Michael Buch <michaelbuch12@gmail.com>
2022-09-21 11:25:03 +0100

    [lldb][test] 8 - Add gmodules XFAIL test for lang/cpp/gmodules/templates

--- lldb/test/API/lang/cpp/gmodules/templates/TestGModules.py
+++ lldb/test/API/lang/cpp/gmodules/templates/TestGModules.py
@@ -1,6 +1,17 @@
-import lldbsuite.test.lldbinline as lldbinline
+import lldb
 from lldbsuite.test.decorators import *
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test import lldbutil
 
-lldbinline.MakeInlineTest(__file__, globals(), [
-    expectedFailureAll(oslist=["linux"], bugnumber="llvm.org/pr36107",
-        debug_info="gmodules")])
+class TestGModules(TestBase):
+
+    @gmodules_test
+    @expectedFailureAll
+    def test_gmodules(self):
+        self.build()
+        lldbutil.run_to_source_breakpoint(self, "Break here", lldb.SBFileSpec("main.cpp"))
+
+        name = lldbutil.frame().FindVariable('m') \
+                .GetChildAtIndex(0).GetChildAtIndex(0).GetChildAtIndex(0).GetName()
+
+        self.assertEqual(name, 'buffer', 'find template specializations in imported modules')

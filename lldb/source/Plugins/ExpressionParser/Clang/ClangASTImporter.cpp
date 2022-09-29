54893bd54508b260c0f40eb02cce171c52a22990
Michael Buch <michaelbuch12@gmail.com>
2022-09-29 14:04:35 +0100

    [lldb][ClangASTImporter] Don't notify subclasses if decls have been already imported

--- lldb/source/Plugins/ExpressionParser/Clang/ClangASTImporter.cpp
+++ lldb/source/Plugins/ExpressionParser/Clang/ClangASTImporter.cpp
@@ -919,8 +919,10 @@ void ClangASTImporter::ASTImporterDelegate::Imported(clang::Decl *from,
         ImporterDelegateSP direct_completer =
             m_main.GetDelegate(&to->getASTContext(), origin.ctx);
 
-        if (direct_completer.get() != this)
+        if (direct_completer.get() != this
+            && !direct_completer->GetAlreadyImportedOrNull(origin.decl) && !direct_completer->getImportedFromDecl(to)) {
           direct_completer->ASTImporter::Imported(origin.decl, to);
+        }
 
         LLDB_LOG(log,
                  "    [ClangASTImporter] Propagated origin "

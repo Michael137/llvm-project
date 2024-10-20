346e98b05056d9c6a82a52f7b7a37636b9e87d4f
Michael Buch <michaelbuch12@gmail.com>
2024-10-20 11:45:40 +0100

    Amend tests

--- lldb/test/Shell/Expr/TestAbiTagCtorsDtors.cpp
+++ lldb/test/Shell/Expr/TestAbiTagCtorsDtors.cpp
@@ -1,20 +1,30 @@
 // Tests that we can call abi-tagged constructors/destructors.
 
 // RUN: %build %s -o %t
-// RUN: %lldb %t -o run -o "expression Tagged()" -o exit | FileCheck %s
+// RUN: %lldb %t -o run \
+// RUN:          -o "expression sinkTagged(getTagged())" \
+// RUN:          -o "expression Tagged()" \
+// RUN:          -o exit | FileCheck %s
 
+// CHECK: expression sinkTagged(getTagged())
 // CHECK: expression Tagged()
-// CHECK: (int) $0 = 15
+
+int global = 0;
 
 struct Tagged {
-  [[gnu::abi_tag("Test", "Tag")]] Tagged() : mem(12) {
+  [[gnu::abi_tag("Test", "CtorTag")]] [[gnu::abi_tag("v1")]] Tagged() = default;
+  [[gnu::abi_tag("Test", "DtorTag")]] [[gnu::abi_tag("v1")]] ~Tagged() = default;
 
-  }
-
-  int mem;
+  int mem = 15;
 };
+
+Tagged getTagged() { return Tagged(); }
+void sinkTagged(Tagged t) {}
 
 int main() {
   Tagged t;
+
+  // TODO: is there a more reliable way of triggering destructor call?
+  sinkTagged(getTagged());
   __builtin_debugtrap();
 }

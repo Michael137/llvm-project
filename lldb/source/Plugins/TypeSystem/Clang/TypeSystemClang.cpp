ba4cdce9d68dc313cb3df9b5df23c3df07207042
Michael Buch <michaelbuch12@gmail.com>
2025-09-02 17:14:50 +0100

    fixup

--- lldb/source/Plugins/TypeSystem/Clang/TypeSystemClang.cpp
+++ lldb/source/Plugins/TypeSystem/Clang/TypeSystemClang.cpp
@@ -2251,7 +2251,7 @@ FunctionDecl *TypeSystemClang::CreateFunctionDeclarationForTemplate(clang::DeclC
               ClangUtil::GetQualType(type).getTypePtr()));
       const auto params = CreateParameterDeclarations(
           func_decl, *prototype, {"T"});
-      func_decl->setParams(params);
+      generic_func_decl->setParams(params);
   }
       //clang::SubstTemplateTypeParmType
 

34ce53a0e73bba74907a5ec3531578a759531d7a
Michael Buch <michaelbuch12@gmail.com>
2025-11-29 13:21:29 +0900

    [lldb][API] Remove null-guards in favour of assertions

--- lldb/source/API/SBMutex.cpp
+++ lldb/source/API/SBMutex.cpp
@@ -48,22 +48,25 @@ bool SBMutex::IsValid() const {
 void SBMutex::lock() const {
   LLDB_INSTRUMENT_VA(this);
 
-  if (m_opaque_sp)
-    m_opaque_sp->lock();
+  assert (m_opaque_sp);
+
+  m_opaque_sp->lock();
 }
 
 void SBMutex::unlock() const {
   LLDB_INSTRUMENT_VA(this);
 
-  if (m_opaque_sp)
-    m_opaque_sp->unlock();
+  assert (m_opaque_sp);
+
+  m_opaque_sp->unlock();
 }
 
 bool SBMutex::try_lock() const {
   LLDB_INSTRUMENT_VA(this);
 
-  if (m_opaque_sp)
-    return m_opaque_sp->try_lock();
+  assert (m_opaque_sp);
+
+  return m_opaque_sp->try_lock();
 
   return false;
 }
